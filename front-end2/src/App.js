a591d54d07b17ee418cc059177b340e48bc0b394
GAURAV DIXIT <202217b3623@wilp.bits-pilani.ac.in>
2023-07-13 15:46:27 +0000

    First commit

--- front-end2/src/App.js
+++ front-end2/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar/Navbar";
 import LoginPage from "./components/Login/Login";
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
 axios.defaults.withCredentials = true;
 function App() {
   // const user = localStorage.getItem("token");
