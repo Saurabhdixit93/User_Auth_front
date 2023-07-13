21464246a5617e97717dc94ab8113bb68ad5d4a9
GAURAV DIXIT <202217b3623@wilp.bits-pilani.ac.in>
2023-07-13 11:37:15 +0000

    updated backend

--- back-end/Routes/index.js
+++ back-end/Routes/index.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserRoutes = require("./UserRoutes");
-const cors = require("cors");
 
-// router.use(
-//   cors({
-//     credentials: true,
-//     origin: process.env.HOME_URL,
-//     methods:["POST","GET","PUT","DELETE"]
-//   })
-// );
 router.use("/user", UserRoutes);
 
 module.exports = router;
--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
 const cookiParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
-const passport = require("passport");
-const passportLocal = require("passport-local");
-const passportLocalConfig = require("./Configration/Passport-local");
 const Routes = require("./Routes/index");
 const { ConnectDb } = require("./Configration/Database");
 const PORT = process.env.PORT;
@@ -24,23 +19,6 @@ app.use(
 app.use(express.json());
 app.use(cookiParser());
 app.use(express.urlencoded({ extended: false }));
-
-// app.use(
-//   session({
-//     name: process.env.SESSION_NAME,
-//     secret: process.env.JWT_PRIVATE_KEY,
-//     saveUninitialized: false,
-//     resave: false,
-//     cookie: {
-//       maxAge: 1000 * 60 * 100,
-//     },
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(passport.setAuthenticatedUser);
-
 app.use("/", Routes);
 
 ConnectDb().then(() => {
